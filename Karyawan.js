772aebadfc5c573871eb032fa0d514a3f1389135
egifadea <92914622+egifadea@users.noreply.github.com>
2022-11-02 12:00:17 +0700

    first commit

---
+++ Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan =[
+    {id:'1', nama:'Egif Adea Lutfi', jabatan:'Bendahara',
+     email:'egifegif568@stimata.com', telp:'081546240013',
+    gaji:'1000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'Rika Nurhalimah ', jabatan:'Sekretaris',
+     email:'rika@stimata.com', telp:'08123456789',
+    gaji:'1000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'3', nama:'Rio Panggalih Putra', jabatan:'Ketua',
+     email:'rioputra@stimata.com', telp:'081345623451',
+    gaji:'1000.000', foto:'https://placeimg.com/100/100/teach'},
+
+    {id:'4', nama:'Putri Yeni', jabatan:'Wakil Ketua',
+     email:'putriyeni@stimata.com', telp:'0857643829130',
+    gaji:'1000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'5', nama:'Egif Adea', jabatan:'Anggota',
+     email:'egif_20510005@stimata.ac.id', telp:'08948362342',
+    gaji:'1000.000', foto:'https://placeimg.com/100/100/any/grayscale'},
+]
+
+export default karyawan;
